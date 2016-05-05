d7c9ec475a663b5eade8ce76467e7ed2f7536ff4
OctavioMaia <octaviojmaia@gmail.com>
2016-05-05 16:29:59 +0100

    cenas

---
+++ Java/src/source/Main.java
@@ -0,0 +1,30 @@
+package source;
+
+import static java.lang.System.out;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Main {
+	
+	public static ArrayList<String> readLinesWithBuff(String fich) {
+	      ArrayList<String> linhas = new ArrayList<>();
+	      BufferedReader inStream = null; 
+	      String linha = null;
+	      try {
+	            inStream = new BufferedReader(new FileReader(fich));
+	            while( (linha = inStream.readLine()) != null )
+					              linhas.add(linha);
+	      }
+	      catch(IOException e){
+	    	  out.println(e.getMessage()); 
+	    	  return null; 
+	      };
+	      return linhas;  
+	   }
+	
+	public static void main(String[] args) {
+		ArrayList<String> teste = readLinesWithBuff("Vendas_3M.txt");
+	}
+	
+}
