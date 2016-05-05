5f9bd4baacfdc0c302407edacb0f7540f5cb4635
OctavioMaia <octaviojmaia@gmail.com>
2016-05-05 16:43:04 +0100

    ja le ficheiro

--- Java/src/source/Main.java
+++ Java/src/source/Main.java
@@ -6,25 +6,30 @@ import java.nio.file.*;
 import java.util.*;
 
 public class Main {
-	
+
 	public static ArrayList<String> readLinesWithBuff(String fich) {
-	      ArrayList<String> linhas = new ArrayList<>();
-	      BufferedReader inStream = null; 
-	      String linha = null;
-	      try {
-	            inStream = new BufferedReader(new FileReader(fich));
-	            while( (linha = inStream.readLine()) != null )
-					              linhas.add(linha);
-	      }
-	      catch(IOException e){
-	    	  out.println(e.getMessage()); 
-	    	  return null; 
-	      };
-	      return linhas;  
-	   }
-	
-	public static void main(String[] args) {
-		ArrayList<String> teste = readLinesWithBuff("Vendas_3M.txt");
+		ArrayList<String> linhas = new ArrayList<>();
+		BufferedReader inStream = null;
+		String linha = null;
+		try {
+			inStream = new BufferedReader(new FileReader(fich));
+			while ((linha = inStream.readLine()) != null)
+				linhas.add(linha);
+		} catch (IOException e) {
+			out.println(e.getMessage());
+			return null;
+		}
+		return linhas;
 	}
-	
+
+	public static void main(String[] args) {
+		Crono.start();
+		
+		ArrayList<String> teste = new ArrayList<>();
+		teste = readLinesWithBuff("src/data/Vendas_3M.txt");
+		
+		Crono.stop();
+		out.println(Crono.print());
+	}
+
 }
